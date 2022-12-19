4eb0728b1654d231040028e44189681492d441c7
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-12-19 13:12:37 +0100

    Add ranking and profile pages

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'profile')]
-    public function index(): Response
+    #[Route('/profile/{username}', name: 'profile')]
+    public function index(UserRepository $userRepository, string $username): Response
     {
+        $user = $userRepository->findOneBy(['username' => $username]);
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user
         ]);
     }
 }
