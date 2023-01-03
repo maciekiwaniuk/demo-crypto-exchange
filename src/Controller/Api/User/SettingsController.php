5ab7149656f8673c9ae8d325668b715435e13707
macieeek <iwaniukmaciej.kontakt@gmail.com>
2023-01-03 13:23:33 +0100

    Update configuration of security

--- src/Controller/Api/User/SettingsController.php
+++ src/Controller/Api/User/SettingsController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller\Api\User;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/user', name: 'api.')]
+#[Route('/api/user', name: 'api.user.')]
 class SettingsController extends AbstractController
 {
-    #[Route('/change_passowrd', name: 'change_password', methods: ['POST'])]
-    public function changePassword(): Response
+    #[Route('/change_password', name: 'change_password', methods: ['POST'])]
+    public function changePassword(LoggerInterface $logger): Response
     {
+        $logger->debug("1", ["test"]);
         // error with entry authentication point
         return $this->json([
             'success' => true,
