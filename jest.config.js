f90f750cf642f0a4c44a81255f6e643357c21a9a
macieeek <iwaniukmaciej.kontakt@gmail.com>
2023-01-31 22:58:22 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
     verbose: true,
     moduleFileExtensions: [
         'js', 'jsx', 'ts', 'vue'
@@ -23,4 +22,4 @@ const config: Config = {
     },
 };
 
-export default config;
+module.exports = config;
