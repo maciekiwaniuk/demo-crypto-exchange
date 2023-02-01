49f9964425ad0c93b3e9dbc6be11c7ca1afd879a
macieeek <iwaniukmaciej.kontakt@gmail.com>
2023-02-01 18:15:07 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 let tailwindcss = require('tailwindcss');
 
 module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    require('postcss-import'),
-    require('autoprefixer')
-  ]
+    plugins: [
+        tailwindcss('./tailwind.config.js'),
+        require('postcss-import'),
+        require('autoprefixer')
+    ]
 }
