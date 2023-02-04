de62acbe35c3562e0df1f8bb9b48f06340236237
macieeek <iwaniukmaciej.kontakt@gmail.com>
2023-02-04 18:58:22 +0100

    Fix typescript problem by updating ts-loader

--- assets/tests/unit/pages/user/Settings.spec.ts
+++ assets/tests/unit/pages/user/Settings.spec.ts
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils';
 import Settings from '../../../../frontend/pages/user/Settings/Settings.vue';
+import { expect } from '@jest/globals';
 
 describe('User - Settings', () => {
     const wrapper = mount(Settings);
